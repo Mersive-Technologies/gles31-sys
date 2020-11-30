cfcfc4f4ea9f23355aa68434c1f0e4aed60b6297
Brent Gardner <bgardner@squarelabs.net>
2020-11-30 12:01:29 -0700

    Run bindgen and commit result

--- src/bindings.rs
+++ src/bindings.rs
@@ -862,6 +862,11 @@ extern "C" {
     pub fn android_get_application_target_sdk_version() -> ::std::os::raw::c_int;
 }
 extern "C" {
+    #[doc = " Returns the API level of the device we're actually running on, or -1 on failure."]
+    #[doc = " The returned values correspond to the named constants in `<android/api-level.h>`,"]
+    #[doc = " and is equivalent to the Java `Build.VERSION.SDK_INT` API."]
+    #[doc = ""]
+    #[doc = " See also android_get_application_target_sdk_version()."]
     pub fn android_get_device_api_level() -> ::std::os::raw::c_int;
 }
 pub type size_t = ::std::os::raw::c_ulong;

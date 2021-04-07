691a26f220025f77c62fe61200fff6e3b8acc1f1
Vladas Zakrevksis <146100@gmail.com>
2021-04-07 14:11:25 +0300

    ios build

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg(target_os = "android")]
+#![cfg(any(target_os = "android", target_os = "ios"))]
 #![allow(dead_code, non_snake_case, non_camel_case_types, non_upper_case_globals)]
 #[allow(improper_ctypes)]
 

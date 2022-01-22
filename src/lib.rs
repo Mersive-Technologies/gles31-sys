3ede088e855070c3255e48844b5927410e74d260
Vladas Zakrevskis <146100@gmail.com>
2022-01-22 19:01:52 +0200

    iOS support
    install lipo
    
    generate bindings func
    
    
    proper iOS/Android check
    
    
    Fixed android linker error
    
    
    iOS bindgen
    
    
    Build stage
    
    
    Delete and ignore bindings
    
    
    Check if symlink exists
    
    
    Android linker explanation
    
    
    Reworked iOS includes
    
    
    Updated readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-#![cfg(target_os = "android")]
-#![allow(dead_code, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+#![cfg(any(target_os = "android", target_os = "ios"))]
+#![allow(
+    dead_code,
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals
+)]
 #[allow(improper_ctypes)]
-
 pub mod bindings;
 pub use bindings::*;
